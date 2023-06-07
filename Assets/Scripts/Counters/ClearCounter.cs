9775136fabd2a8c488db9d21bae33a0fc391cca6
SicariusKun <92648924+SicariusKun@users.noreply.github.com>
2023-06-07 17:06:56 +0200

    test

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -23,7 +23,22 @@ public class ClearCounter : BaseCounter
         {
             if (player.HasKitchenObject())
             {
-
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+                    }
+                }
+                else
+                {
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject))
+                    {
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             }
             else
             {
@@ -31,5 +46,4 @@ public class ClearCounter : BaseCounter
             }
         }
     }
-
 }
