b847ace1dfe75c9d0a50de30a6fe286c397347a1
SicariusKun <92648924+SicariusKun@users.noreply.github.com>
2023-06-21 13:00:26 +0200

    jgj

---
+++ Assets/Scripts/UI/DeliveryResultUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DeliveryResultUI : MonoBehaviour
+{
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI messageText;
+    [SerializeField] private Color successColor;
+    [SerializeField] private Color failedColor;
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failedSprite;
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
+    {
+        backgroundImage.color = successColor;
+        iconImage.sprite = successSprite;
+        messageText.text = "DELIVERY\nSUCCESS";
+    }
+
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
+    {
+        backgroundImage.color = failedColor;
+        iconImage.sprite = failedSprite;
+        messageText.text = "DELIVERY\nFAILED";
+    }
+}
